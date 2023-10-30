c34d619523136f6263710cfc1115b43948f81619
codermahdi <mahdihassan.bd18@gmail.com>
2023-10-30 11:42:23 +0000

    add new doctype

---
+++ excel_hr/excel_hr/doctype/excel_credit_limit_enhancement_form/excel_credit_limit_enhancement_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Shaid Azmin and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Excel Credit Limit Enhancement Form', {
+	// refresh: function(frm) {
+
+	// }
+});

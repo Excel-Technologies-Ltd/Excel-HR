89115714915d8aafd56a674cad7e68a653f5be4e
Shaid Azmin <excel.azmin@gmail.com>
2025-06-22 09:06:22 +0000

    update: Excel Job Location filed on Excel Leave Analysis  Report

--- excel_hr/excel_hr/report/excel_leave_analysis/excel_leave_analysis.py
+++ excel_hr/excel_hr/report/excel_leave_analysis/excel_leave_analysis.py
@@ -93,10 +93,10 @@ def get_conditions(filters):
         conditions.update({"excel_hr_section": filters.get("excel_section")})
     if filters.get("excel_sub_section"):
         conditions.update({"excel_hr_sub_section": filters.get("excel_sub_section")})
-    if filters.get("excel_job_location"):
-        conditions.update({"excel_job_location": filters.get("excel_job_location")})
-    if filters.get("excel_reporting_location"):
-        conditions.update({"excel_reporting_location": filters.get("excel_reporting_location")})        
+    if filters.get("custom_job_location"):
+        conditions.update({"custom_job_location": filters.get("custom_job_location")})
+    if filters.get("custom_reporting_location"):
+        conditions.update({"custom_reporting_location": filters.get("custom_reporting_location")})        
     return conditions
 
 def get_data(filters, leave_types, column_name_prefix=""):

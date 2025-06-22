c97ae973ed91a181139ac3787bcf2bf592f5915b
Shaid Azmin <excel.azmin@gmail.com>
2025-06-22 09:15:03 +0000

    update: Excel Job Location filed on Excel Employee Utilized Leave Summary Report

--- excel_hr/excel_hr/report/excel_employee_utilized_leave_summary/excel_employee_utilized_leave_summary.py
+++ excel_hr/excel_hr/report/excel_employee_utilized_leave_summary/excel_employee_utilized_leave_summary.py
@@ -67,12 +67,12 @@ def get_conditions(filters):
     if filters.get("excel_sub_section"):
         conditions.update(
             {"excel_hr_sub_section": filters.get("excel_sub_section")})
-    if filters.get("excel_job_location"):
+    if filters.get("custom_job_location"):
         conditions.update(
-            {"excel_job_location": filters.get("excel_job_location")})
-    if filters.get("excel_reporting_location"):
+            {"custom_job_location": filters.get("custom_job_location")})
+    if filters.get("custom_reporting_location"):
         conditions.update(
-            {"excel_reporting_location": filters.get("excel_reporting_location")})
+            {"custom_reporting_location": filters.get("custom_reporting_location")})
     return conditions
 
 

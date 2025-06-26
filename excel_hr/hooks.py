92c208a221431f12ae681fdac471386ac1441fc0
Shaid Azmin <excel.azmin@gmail.com>
2025-06-26 08:01:57 +0000

    feature: Employee On Absent sent mail to employee and leave_approver

--- excel_hr/hooks.py
+++ excel_hr/hooks.py
@@ -152,6 +152,9 @@ override_doctype_class = {
     "Employee": "excel_hr.overrides.UserWithEmployee",
     "Attendance Request": "excel_hr.attendance_request.NewAttendanceRequest",
     "Attendance Request": "excel_hr.overrides.CustomAttendanceRequest",
+    "Leave Application": "excel_hr.overrides.EnabledDateValidation",
+    "Leave Application": "excel_hr.overrides.EnabledDayValidation",
+    
 }
 
 # Document Events
--- excel_hr/overrides.py
+++ excel_hr/overrides.py
@@ -1,93 +1,142 @@
 import frappe
 from erpnext.setup.doctype.employee.employee import Employee
 from hrms.hr.doctype.attendance_request.attendance_request import AttendanceRequest
+from hrms.hr.doctype.leave_application.leave_application import LeaveApplication
 from frappe.model.document import Document
 from frappe.utils import getdate, nowdate, format_date
 from datetime import datetime, timedelta
 from frappe import _
 
-class CustomLeaveApplication(Document):
+class EnabledDayValidation(LeaveApplication):
+    
     def validate(self):
-        self.validate_posting_date_range()
-        self.validate_annual_leave_balance()
+        aleart_doc=frappe.get_doc("ArcHR Settings")
+        if aleart_doc.enabled_day_validation_annual_leave == 1:
+            self.validate_annual_leave_balance()
+        
+    def validate_annual_leave_balance(self):
+        if not self.employee:
+            frappe.throw(_("Please select an employee."))
+            return
+
+        # Step 1: Fetch the annual leave allocation for the employee
+        leave_details = self.get_leave_allocation_details()
+        if not leave_details:
+            frappe.throw(_("Could not fetch leave allocation details."))
+            return
+
+        # Step 2: Query previous leave applications within the same year
+        current_year = datetime.now().year
+        existing_leaves = self.get_existing_leaves(current_year)
+
+        # Calculate new leave days
+        new_leave_days = self.calculate_leave_days()
+        if new_leave_days is None:
+            return
+
+        # Step 3: Sum the leave days from previous applications
+        total_used_leaves = sum(leave["total_leave_days"] for leave in existing_leaves)
+        
+        # Step 4: Check if the total exceeds the allocated annual leave
+        total_after_new = total_used_leaves + new_leave_days
+        if total_after_new > leave_details:
+            frappe.throw(_(
+                "<b>Total of the leave days exceeds your current allowance.</b> "
+                "Please reduce the number of days or select a different leave type."
+            ))
+
+    def get_leave_allocation_details(self):
+        """Get the total allocated annual leaves for the employee"""
+        try:
+            leave_details = frappe.get_doc("Leave Allocation", {
+                "employee": self.employee,
+                "leave_type": "Annual Leave",
+                "docstatus": 1
+            })
+            return leave_details.total_leaves_allocated
+        except Exception:
+            return None
+
+    def get_existing_leaves(self, year):
+        """Get all approved/annual leaves for the employee in the given year"""
+        return frappe.get_all("Leave Application",
+            filters=[
+                ["employee", "=", self.employee],
+                ["leave_type", "=", "Annual Leave"],
+                ["from_date", ">=", f"{year}-01-01"],
+                ["from_date", "<=", f"{year}-12-31"],
+                ["status", "in", ["Open", "Approved"]],
+                ["name", "!=", self.name]  # Exclude current document if updating
+            ],
+            fields=["total_leave_days"]
+        )
+
+    def calculate_leave_days(self):
+        """Calculate the number of leave days in the current application"""
+        if not self.from_date or not self.to_date:
+            return None
+            
+        from_date = datetime.strptime(self.from_date, "%Y-%m-%d").date()
+        to_date = datetime.strptime(self.to_date, "%Y-%m-%d").date()
+        return (to_date - from_date).days + 1
+    
+
+class EnabledDateValidation(LeaveApplication):
+    def validate(self):
+        aleart_doc=frappe.get_doc("ArcHR Settings")
+        if aleart_doc.enabled_date_validation == 1:
+            self.validate_posting_date_range()
 
     def validate_posting_date_range(self):
         if not self.posting_date:
             return
 
+        # Convert string dates to datetime objects
         posting_date = datetime.strptime(self.posting_date, "%Y-%m-%d").date()
         current_date = datetime.now().date()
-        current_month = current_date.month
+        current_month = current_date.month  # Current month (1-12)
         current_year = current_date.year
 
         # Get month names
         last_month_date = datetime(current_year, current_month - 1, 1)
-        last_month_name = last_month_date.strftime('%B')
+        last_month_name = last_month_date.strftime('%B')  # Full month name
+
         current_month_name = datetime(current_year, current_month, 1).strftime('%B')
 
+        if not self.from_date:
+            return
+            
+        from_date = datetime.strptime(self.from_date, "%Y-%m-%d").date()
+
+        # Check if posting date is between 1st and 25th of the current month
         if posting_date.day >= 1 and posting_date.day <= 25:
-            # Check if from_date is between last month's 21st and current month's 20th
-            range_start = datetime(current_year, current_month - 1, 21).date()
-            
-            from_date = datetime.strptime(self.from_date, "%Y-%m-%d").date() if self.from_date else None
-            
-            if from_date and from_date < range_start:
-                if from_date > posting_date:
-                    return
-                frappe.throw(f'The maximum "From Date" <b>21st {last_month_name} {current_year}</b> is allowed.')
-                
+            # Set from_date range to last month's 21st to current month's 20th
+            range_start = datetime(current_year, current_month - 1, 21).date()  # 21st of last month
+            range_end = datetime(current_year, current_month, 25).date()  # 25th of current month
+
+            # Skip validation if from_date is after posting_date
+            if from_date > posting_date:
+                return
+
+            if from_date < range_start:
+                frappe.throw(
+                    _('The maximum "From Date" <b>21st {0} {1}</b> is allowed.').format(
+                        last_month_name, current_year
+                    )
+                )
+
+        # Check if posting date is between 26th and 31st of the current month
         elif posting_date.day >= 26 and posting_date.day <= 31:
-            # Check if from_date is between current month's 21st and last day of month
+            # Set from_date range to 21st of current month to last day of month
             range_start = datetime(current_year, current_month, 21).date()
             last_day_of_month = (datetime(current_year, current_month + 1, 1) - timedelta(days=1)).date()
-            
-            from_date = datetime.strptime(self.from_date, "%Y-%m-%d").date() if self.from_date else None
-            
-            if from_date and (from_date < range_start or from_date > last_day_of_month):
-                frappe.throw(f'The maximum "From Date" <b>21st {current_month_name} {current_year}</b> is allowed.')
 
-    def validate_annual_leave_balance(self):
-        if not self.employee:
-            frappe.throw('Please select an employee.')
-            return
-
-        # Get leave allocation details
-        leave_details = frappe.get_doc("Leave Allocation", {
-            "employee": self.employee,
-            "leave_type": "Annual Leave",
-            "docstatus": 1
-        })
-
-        if not leave_details:
-            frappe.throw('Could not fetch leave allocation details.')
-            return
-
-        total_allocated_leaves = leave_details.total_leaves_allocated
-
-        # Calculate new leave days
-        from_date = datetime.strptime(self.from_date, "%Y-%m-%d").date()
-        to_date = datetime.strptime(self.to_date, "%Y-%m-%d").date()
-        new_leave_days = (to_date - from_date).days + 1
-
-        # Get existing approved leaves for the year
-        current_year = datetime.now().year
-        existing_leaves = frappe.get_all("Leave Application",
-            filters=[
-                ["employee", "=", self.employee],
-                ["leave_type", "=", "Annual Leave"],
-                ["from_date", ">=", f"{current_year}-01-01"],
-                ["from_date", "<=", f"{current_year}-12-31"],
-                ["status", "in", ["Open", "Approved"]]
-            ],
-            fields=["from_date", "to_date", "total_leave_days"]
-        )
-
-        total_used_leaves = sum(leave.total_leave_days for leave in existing_leaves)
-        total_after_new = total_used_leaves + new_leave_days
-
-        if total_after_new > total_allocated_leaves:
-            frappe.throw('<b>Total of the leave days exceeds your current allowance.</b> Please reduce the number of days or select a different leave type.')
-
+            if from_date < range_start or from_date > last_day_of_month:
+                frappe.throw(
+                    _('The maximum "From Date" <b>21st {0} {1}</b> is allowed.').format(
+                        current_month_name, current_year
+                    )
+                )
 
 class CustomAttendanceRequest(AttendanceRequest):
     def before_save(self):
